d1a9d1d9dffe94b7891280275a07dcb7dab1c3f1
Nawaf-Aljuhaimi <115425834+Nawaf-Aljuhaimi@users.noreply.github.com>
2022-10-11 21:45:15 +0300

    InsertionSort Method

--- Sorter.java
+++ Sorter.java
@@ -13,5 +13,19 @@ static void selectionSort(int arr[]) {
 			}
 		}
 	}
-  
+  private int[] insertionSort(int[] input){
+		
+	    int temp;
+		
+	    for (int i = 1; i < input.length; i++) {
+		    for(int j = i ; j > 0 ; j--){
+			    if(input[j] < input[j-1]){
+				    temp = input[j];
+				    input[j] = input[j-1];
+				    input[j-1] = temp;
+			    }
+		    }
+	    }
+	    return input;
+	}
 }
